456ceaa9266f77aaaedb718aef7ac88d952f2f08
Eyshcharya <eyshcharya@gmail.com>
2023-10-09 17:32:11 +0530

    Refactored the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,3 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <p> Hello World</p>
-    </main>
-  );
+  return <h1>Home Page</h1>;
 }
