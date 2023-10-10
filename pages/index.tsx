7493f92993e61b1d191e210425d0b163d3434aec
Eyshcharya <eyshcharya@gmail.com>
2023-10-10 12:16:41 +0530

    Implemented Programmatical Navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 export default function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log(`Placing your Order`);
+    router.push(`/product`);
+    // router.replace(`/product`);
+  };
   return (
     <>
       <h1>Home Page</h1>
@@ -12,6 +19,7 @@ export default function Home() {
       <Link href={`/docs`}>
         <p>Docs</p>
       </Link>
+      <button onClick={handleClick}>Place Order</button>
     </>
   );
 }
