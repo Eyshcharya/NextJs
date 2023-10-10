54014bdd7e98c9a26e5eb445c1d5d66ed0570482
Eyshcharya <eyshcharya@gmail.com>
2023-10-10 11:41:35 +0530

    Implemented Link Component for Navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link";
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Link href={`/blog`}>
+        <p>Blog</p>
+      </Link>
+      <Link href={`/product`}>
+        <p>Products</p>
+      </Link>
+      <Link href={`/docs`}>
+        <p>Docs</p>
+      </Link>
+    </>
+  );
 }
--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,11 +1,19 @@
+import Link from "next/link";
 const productList = () => {
   const productArray = ["Product 1", "Product 2", "Product 3"];
   return (
     <>
       <h1>productList</h1>
       {productArray.map((product, index) => {
-        return <h2 key={index}>{product}</h2>;
+        return (
+          <h2 key={index}>
+            <Link href={`/product/${index + 1}`}>{product}</Link>
+          </h2>
+        );
       })}
+      <Link href={`/`}>
+        <p>Home</p>
+      </Link>
     </>
   );
 };
