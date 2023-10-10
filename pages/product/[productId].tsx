512916b4eefc3f709e32cefc6f5b6f8d880ee0d9
Eyshcharya <eyshcharya@gmail.com>
2023-10-10 09:38:50 +0530

    Refactored the code

--- pages/product/[productId].tsx
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router";
-const singleProduct = () => {
-  const router = useRouter();
-  const productId = router.query.productId;
-  return <h1> Single Product {productId}</h1>;
-};
-export default singleProduct;
