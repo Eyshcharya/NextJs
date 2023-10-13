1994ab1e65e1d75c95415960f9e28c78c734c857
Eyshcharya <eyshcharya@gmail.com>
2023-10-13 11:34:52 +0530

    Refactored the code

--- Pre-Rendering/ssr/pages/index.tsx
+++ Pre-Rendering/ssr/pages/index.tsx
@@ -1,13 +1,11 @@
-import { useRouter } from "next/router";
 import Link from "next/link";
 const Home = () => {
-  const router = useRouter();
   return (
     <>
-      <h1>Next Js Pre-rendering</h1>
+      <h1>Next Js Pre-Rendering</h1>
 
-      <Link href={`/posts`}>
-        <h3>{`Posts >>>`} </h3>
+      <Link href={`/products`}>
+        <h3>{`Products >>>`} </h3>
       </Link>
     </>
   );
