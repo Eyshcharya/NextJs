96bf09e3b65d5b780b143171550bb50a4becfb64
Eyshcharya <eyshcharya@gmail.com>
2023-10-13 18:04:09 +0530

    Implementing Data

---
+++ Next-Api/data/comments.js
@@ -0,0 +1,18 @@
+export const Comments = [
+  {
+    id: 1,
+    text: "This is the first object with a short text.",
+  },
+  {
+    id: 2,
+    text: "Here's the second object's text, which is also short.",
+  },
+  {
+    id: 3,
+    text: "The third object's text is within the 20-word limit.",
+  },
+  {
+    id: 4,
+    text: "This is the fourth object with a brief description.",
+  },
+];
