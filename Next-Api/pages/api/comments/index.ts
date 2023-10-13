75f4c51f8a9c501f49905832aefaf1ee1eda5889
Eyshcharya <eyshcharya@gmail.com>
2023-10-13 18:03:29 +0530

    Implementing API endpoints

--- Next-Api/pages/api/comments/index.ts
+++ Next-Api/pages/api/comments/index.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Comments } from "../../../data/comments";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: `Home API route` });
+  res.status(200).json(Comments);
 }
