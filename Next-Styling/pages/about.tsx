29c578c8f12f155a04dcbbb7ea466a64eb16fea7
Eyshcharya <eyshcharya@gmail.com>
2023-10-16 14:03:52 +0530

    Added New Styles

--- Next-Styling/pages/about.tsx
+++ Next-Styling/pages/about.tsx
@@ -1,4 +1,8 @@
 const about = () => {
-  return <h2>About</h2>;
+  return (
+    <div className='mt-40'>
+      <h2 className='text-3xl font-bold underline'>About</h2>;
+    </div>
+  );
 };
 export default about;
