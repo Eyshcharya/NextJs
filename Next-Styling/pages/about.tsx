8818d29f5f402623de8feca09062e891bdb12f9f
Eyshcharya <eyshcharya@gmail.com>
2023-10-16 14:15:49 +0530

    Added Styles with CSS Modules and Tailwind CSS

--- Next-Styling/pages/about.tsx
+++ Next-Styling/pages/about.tsx
@@ -1,7 +1,10 @@
+import styles from "../styles/About.module.css";
+
 const about = () => {
   return (
-    <div className='mt-40'>
-      <h2 className='text-3xl font-bold underline'>About</h2>;
+    <div className='mt-10'>
+      <h2 className='text-3xl font-bold underline'>About</h2>
+      <div className={styles.highlight}>0112345678</div>
     </div>
   );
 };
